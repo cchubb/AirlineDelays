962590094fd02e7aa4f36b31172d761865d735ca
Chris Chubb <cchubb@codegurus.com>
2017-02-22 23:43:21 -0500

    Ignoring downloaded data.

--- prepare_data.R
+++ prepare_data.R
@@ -2,17 +2,19 @@
 # Prepare the flight data set
 # Plot just for delays associated with certain airports in Florida using certain carriers
 
+setwd("C:/Users/cchubb/Coursera/DataScience/9DevelopingDataProducts/Project/AirlineDelays")
+
 library(dplyr)
 require(ggplot2)
 
 #Original flight data downloaded from http://stat-computing.org/dataexpo/2009/the-data.html
 
 if (! file.exists("data/2008.csv.bz2")) {
-  download.file("data/2008.csv.bz2", "http://stat-computing.org/dataexpo/2009/2008.csv.bz2")
+  download.file("http://stat-computing.org/dataexpo/2009/2008.csv.bz2", "data/2008.csv.bz2")
 }
 
 if (! file.exists("data/carriers.csv")) {
-  download.file("data/carriers.csv", "http://stat-computing.org/dataexpo/2009/carriers.csv")
+  download.file("http://stat-computing.org/dataexpo/2009/carriers.csv", "data/carriers.csv")
 }
 
 raw_2008 <- read.csv("data/2008.csv.bz2")
@@ -34,9 +36,9 @@ delays$Name <- sub("([A-Za-z]+).*", "\\1", delays$Description) #Short airline na
 
 saveRDS(delays, "delays.rds")
 
-#A test plot
-scale_color_hue(l=90) +
+rm(raw_2008, carriers) #Clean up
 
+#A test plot
 p <- ggplot(delays, aes(x=Origin, y=DepDelay)) +
   geom_boxplot() +
   geom_jitter(shape=16, size=2, position=position_jitter(0.2), alpha=0.6, aes(color=Name))
